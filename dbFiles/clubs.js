be5ec0edebcd181b679463d59b9d3d1f4dc2c814
Ryan Kasper <rhinodino5628@gmail.com>
2022-12-03 15:15:07 -0600

    Modified register window and added another class.

--- dbFiles/clubs.js
+++ dbFiles/clubs.js
@@ -8,3 +8,4 @@ dbToWS.on("error", console.error.bind(console, "MongoDB had an error."));
 
 const club1 = require("./club1"); // Need to figure out how to search the data base for commonalities
 const club2 = require("./club2");
+const clubcollection = require("./clubcollection");

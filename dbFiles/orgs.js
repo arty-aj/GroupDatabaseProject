d49c8a39514cb6ba24db56e335f95d8c6a91329d
Ryan Kasper <rhinodino5628@gmail.com>
2022-12-04 16:14:54 -0600

    Updated the usage of the orgs.

--- dbFiles/orgs.js
+++ dbFiles/orgs.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.IMONG);
@@ -6,6 +7,10 @@ const dbToWS = mongoose.connection;
 
 dbToWS.on("error", console.error.bind(console, "MongoDB had an error."));
 
-const org1 = require("./"); // Need to figure out how to search the data base for commonalities
-const org2 = require("./");
-const orgcollection = require("./");
+const org = require("./"); // Need to figure out how to search the data base for commonalities
+
+const getAllorgs = (response) =>{
+    org.getAllorgs(response);
+};
+
+//const indivOrgs = 

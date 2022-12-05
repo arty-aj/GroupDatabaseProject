c4c79b7cd6cd1a5499be014487739e84e43e9a66
acoro013 <carturo06@gmail.com>
2022-12-05 15:35:25 -0600

    cards

--- app.js
+++ app.js
@@ -29,8 +29,11 @@ app.get("/orgPage", (req, res) => {
   res.render("orgPage");
 });
 
+
 app.listen(port, () => console.info("Listening on port", { port }));
 //localhost:3000
+
+
 
 /*router.post("/submitInformation", (req, res) => {
   const contact = new contacts({

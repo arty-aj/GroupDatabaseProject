0d79f4982854ca3ace44b154392a8732e6c0f3cf
Ryan Kasper <rhinodino5628@gmail.com>
2022-12-05 11:57:40 -0600

    Decided to change my plan. Nothing new was added.

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + '/public/css'))
-app.use('/img', express.static(__dirname + '/public/img'))
-app.set('views', './views')
-app.set('view engine', 'ejs')
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "/public/css"));
+app.use("/img", express.static(__dirname + "/public/img"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/explorePage', (req, res) => {
-    res.render('explorePage')
-})
+app.get("/explorePage", (req, res) => {
+  res.render("explorePage");
+});
 
-app.get('/register', (req, res) => {
-    res.render('register')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get('/registerClub', (req, res) => {
-    res.render('registerClub')
-})
+app.get("/registerClub", (req, res) => {
+  res.render("registerClub");
+});
 
-app.get('/orgPage', (req, res) => {
-    res.render('orgPage')
-})
+app.get("/orgPage", (req, res) => {
+  res.render("orgPage");
+});
 
-app.listen(port, () => console.info('Listening on port', {port}))
+app.listen(port, () => console.info("Listening on port", { port }));
 //localhost:3000
